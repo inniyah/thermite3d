e2fb827a7c89366e3928948c3fb2ea71bb7d90a5
David Williams <david@david-williams.info>
2009-05-21 22:44:19 +0000

    Renamed VlumeIterator to VolumeSampler.

--- Thermite/source/VolumeSerializer.cpp
+++ Thermite/source/VolumeSerializer.cpp
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "VolumeSerializer.h"
 #include "Volume.h"
 
-#include "VolumeIterator.h"
+#include "VolumeSampler.h"
 
 #include "Serialization.h"
 
