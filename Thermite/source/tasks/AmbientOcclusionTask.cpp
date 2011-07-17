72e2afc0be86c65e46e8a2d4fdcb1dffc858f27e
David Williams <david@thermite3d.org>
2011-07-18 00:06:33 +0100

    Initial work on an experimental method of computng ambient occlusion from summed area tables.

--- Thermite/source/tasks/AmbientOcclusionTask.cpp
+++ Thermite/source/tasks/AmbientOcclusionTask.cpp
@@ -44,9 +44,150 @@ namespace Thermite
 	
 	void AmbientOcclusionTask::run(void)
 	{	
-		uint8_t uNoOfSamplesPerOutputElement = 0; //Max off 255 for max quality.
+		/*uint8_t uNoOfSamplesPerOutputElement = 4; //Max off 255 for max quality.
 		AmbientOcclusionCalculator<SimpleVolume, Material16> ambientOcclusionCalculator(mVolume, mAmbientOcclusionVolume, m_regToProcess, mRayLength, uNoOfSamplesPerOutputElement);
-		ambientOcclusionCalculator.execute();
+		ambientOcclusionCalculator.execute();*/
+
+		/*for(int32_t oz = mVolume->getEnclosingRegion().getLowerCorner().getZ(); oz < mVolume->getEnclosingRegion().getUpperCorner().getZ(); oz++)
+		{
+			for(int32_t oy = mVolume->getEnclosingRegion().getLowerCorner().getY(); oy < mVolume->getEnclosingRegion().getUpperCorner().getY(); oy++)
+			{
+				for(int32_t ox = mVolume->getEnclosingRegion().getLowerCorner().getX(); ox < mVolume->getEnclosingRegion().getUpperCorner().getX(); ox++)
+				{
+					(*mAmbientOcclusionVolume)[oz][oy][ox] = 0; //FIXME - might need offset here if volume and ambient volume don't both start at (0,0,0)
+
+					int32_t border = 1;
+					for(int32_t iz = oz - border; iz <= oz + border; iz++)
+					{
+						for(int32_t iy = oy - border; iy <= oy + border; iy++)
+						{
+							for(int32_t ix = ox - border; ix <= ox + border; ix++)
+							{
+								if(mVolume->getVoxelAt(ix,iy,iz).getMaterial() == 0) //FIXME - should use density not material
+								{
+									(*mAmbientOcclusionVolume)[oz][oy][ox] += 9; //FIXME - should not be 9
+								}
+							}
+						}
+					}
+				}
+			}
+		}*/
+
+		int32_t border = 5;
+
+		Array<3, uint32_t> SAT(ArraySizes(mVolume->getWidth() + border * 2)(mVolume->getHeight() + border * 2)(mVolume->getDepth() + border * 2));
+
+		//int32_t worldX = mVolume->getEnclosingRegion().getLowerCorner().getX() - border;
+		//int32_t worldY = mVolume->getEnclosingRegion().getLowerCorner().getY() - border;
+		//int32_t worldZ = mVolume->getEnclosingRegion().getLowerCorner().getZ() - border;
+
+		for(int32_t satZ = 0, worldZ = mVolume->getEnclosingRegion().getLowerCorner().getZ() - border; satZ < SAT.getDimension(2); satZ++, worldZ++)
+		{
+			for(int32_t satY = 0, worldY = mVolume->getEnclosingRegion().getLowerCorner().getY() - border; satY < SAT.getDimension(1); satY++, worldY++)
+			{
+				SAT[satZ][satY][0] = mVolume->getVoxelAt(mVolume->getEnclosingRegion().getLowerCorner().getX() - border,worldY,worldZ).getMaterial() == 0 ? 0 : 1;
+				for(int32_t satX = 1, worldX = mVolume->getEnclosingRegion().getLowerCorner().getX() - border + 1; satX < SAT.getDimension(0); satX++, worldX++)
+				{
+					uint32_t isSolid = mVolume->getVoxelAt(worldX,worldY,worldZ).getMaterial() == 0 ? 0 : 1;
+					SAT[satZ][satY][satX] = SAT[satZ][satY][satX-1] + isSolid;
+				}
+			}
+		}
+
+
+		/*worldX = mVolume->getEnclosingRegion().getLowerCorner().getX() - border;
+		worldY = mVolume->getEnclosingRegion().getLowerCorner().getY() - border;
+		worldZ = mVolume->getEnclosingRegion().getLowerCorner().getZ() - border;*/
+
+		for(int32_t satZ = 0; satZ < SAT.getDimension(2); satZ++)
+		{
+			for(int32_t satY = 1; satY < SAT.getDimension(1); satY++)
+			{
+				for(int32_t satX = 0; satX < SAT.getDimension(0); satX++)
+				{
+					SAT[satZ][satY][satX] += SAT[satZ][satY-1][satX];
+				}
+			}
+		}
+
+		for(int32_t satZ = 1; satZ < SAT.getDimension(2); satZ++)
+		{
+			for(int32_t satY = 0; satY < SAT.getDimension(1); satY++)
+			{
+				for(int32_t satX = 0; satX < SAT.getDimension(0); satX++)
+				{
+					SAT[satZ][satY][satX] += SAT[satZ-1][satY][satX];
+				}
+			}
+		}
+
+		for(int32_t ambVolZ = 0; ambVolZ < mAmbientOcclusionVolume->getDimension(2); ambVolZ++)
+		{
+			for(int32_t ambVolY = 0; ambVolY < mAmbientOcclusionVolume->getDimension(1); ambVolY++)
+			{
+				for(int32_t ambVolX = 0; ambVolX < mAmbientOcclusionVolume->getDimension(0); ambVolX++)
+				{
+					int32_t satLowerX = ambVolX;
+					int32_t satLowerY = ambVolY;
+					int32_t satLowerZ = ambVolZ;
+
+					int32_t satUpperX = ambVolX + border * 2;
+					int32_t satUpperY = ambVolY + border * 2;
+					int32_t satUpperZ = ambVolZ + border * 2;
+
+					int32_t a = SAT[satLowerZ][satLowerY][satLowerX];
+					int32_t b = SAT[satLowerZ][satLowerY][satUpperX];
+					int32_t c = SAT[satLowerZ][satUpperY][satLowerX];
+					int32_t d = SAT[satLowerZ][satUpperY][satUpperX];
+					int32_t e = SAT[satUpperZ][satLowerY][satLowerX];
+					int32_t f = SAT[satUpperZ][satLowerY][satUpperX];
+					int32_t g = SAT[satUpperZ][satUpperY][satLowerX];
+					int32_t h = SAT[satUpperZ][satUpperY][satUpperX];
+
+					float noSolid = h+c-d-g-f-a+b+e;
+					float maxSolid = border * 2/* + 1*/;
+					maxSolid = maxSolid * maxSolid * maxSolid;
+
+					float percentSolid = noSolid / maxSolid;
+					float percentEmpty = 1.0f - percentSolid;
+
+					(*mAmbientOcclusionVolume)[ambVolZ][ambVolY][ambVolX] = 255 * percentEmpty;
+
+					//(*mAmbientOcclusionVolume)[ambVolZ][ambVolY][ambVolX] = 255 - ((h+c-d-g-f-a+b+e) * 19); //FIXME - should not be 9
+				}
+			}
+		}
+
+
+
+
+
+		/*for(int32_t oz = mVolume->getEnclosingRegion().getLowerCorner().getZ(); oz < mVolume->getEnclosingRegion().getUpperCorner().getZ(); oz++)
+		{
+			for(int32_t oy = mVolume->getEnclosingRegion().getLowerCorner().getY(); oy < mVolume->getEnclosingRegion().getUpperCorner().getY(); oy++)
+			{
+				for(int32_t ox = mVolume->getEnclosingRegion().getLowerCorner().getX(); ox < mVolume->getEnclosingRegion().getUpperCorner().getX(); ox++)
+				{
+					(*mAmbientOcclusionVolume)[oz][oy][ox] = 0; //FIXME - might need offset here if volume and ambient volume don't both start at (0,0,0)
+
+					
+					for(int32_t iz = oz - border; iz <= oz + border; iz++)
+					{
+						for(int32_t iy = oy - border; iy <= oy + border; iy++)
+						{
+							for(int32_t ix = ox - border; ix <= ox + border; ix++)
+							{
+								if(mVolume->getVoxelAt(ix,iy,iz).getMaterial() == 0) //FIXME - should use density not material
+								{
+									(*mAmbientOcclusionVolume)[oz][oy][ox] += 9; //FIXME - should not be 9
+								}
+							}
+						}
+					}
+				}
+			}
+		}*/
 
 		emit finished(this);
 	}
