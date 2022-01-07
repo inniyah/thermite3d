0c72aa6f993a0771f1e458059b83a5d7c5ad49ff
Miriam Ruiz <miriam@debian.org>
2022-01-07 14:17:34 +0100

    Added old PolyVox code (taken from voxeliens)

---
+++ PolyVox/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/inniyah/code/voxels/voxeliens/dependencies/polyvox
+# Build directory: /home/inniyah/code/voxels/voxeliens/dependencies/polyvox
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("library")
+subdirs("documentation")
